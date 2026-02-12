fb4552ed40a42633a9df82ba5026c21545a87e7e
karnandohuang <karnandohuang@outlook.com>
2026-02-12 11:57:35 +0700

    render dynamic list of core concepts

--- src/App.jsx
+++ src/App.jsx
@@ -30,16 +30,7 @@ function App() {
       <main>
           <section id="core-concepts">
               <h2>Core Concepts</h2>
-              <ul>
-                  <CoreConcept
-                      title={CORE_CONCEPTS[0].title}
-                      description={CORE_CONCEPTS[0].description}
-                      image={CORE_CONCEPTS[0].image}
-                  />
-                  <CoreConcept {...CORE_CONCEPTS[1]} />
-                  <CoreConcept {...CORE_CONCEPTS[2]} />
-                  <CoreConcept {...CORE_CONCEPTS[3]} />
-              </ul>
+              <ul>{CORE_CONCEPTS.map(concept => <CoreConcept {...concept}/>)}</ul>
           </section>
           <section id="examples">
               <h2>Examples</h2>
