8def660cb4e941bdcd5325557f360ff6d986939d
karnandohuang <karnandohuang@outlook.com>
2026-02-12 11:46:40 +0700

    use tabContent variable to store content

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,19 @@ function App() {
         // console.log('Hello World! - Selected', selectedButton)
         setSelectedTopic(selectedButton)
     }
+
+    let tabContent = <p>Please select a topic.</p>
+    if (selectedTopic) {
+        tabContent = (
+            <div id="tab-content">
+                <h3>{EXAMPLES[selectedTopic]?.title}</h3>
+                <p>{EXAMPLES[selectedTopic]?.description}</p>
+                <pre>
+                      <code>{EXAMPLES[selectedTopic]?.code}</code>
+                  </pre>
+            </div>
+        )
+    }
   return (
     <div>
       <Header />
@@ -36,17 +49,7 @@ function App() {
                   <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
                   <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
               </menu>
-              {!selectedTopic ? (
-                  <p>Please select a topic.</p>
-              ) : (
-                  <div id="tab-content">
-                      <h3>{EXAMPLES[selectedTopic]?.title}</h3>
-                      <p>{EXAMPLES[selectedTopic]?.description}</p>
-                      <pre>
-                      <code>{EXAMPLES[selectedTopic]?.code}</code>
-                  </pre>
-                  </div>
-              )}
+              {tabContent}
           </section>
         <h2>Time to get started!</h2>
       </main>
