abb69aefcca3e0983457b41a39052c1e9613281c
karnandohuang <karnandohuang@outlook.com>
2026-02-12 09:55:40 +0700

    add core concepts

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import imageX from "./assets/react-core-concepts.png"
+import componentsImg from './assets/components.png'
+import { CORE_CONCEPTS } from './data'
 
 const reactDescription = ['Fundamental', 'Crucial', 'Core']
 
@@ -19,11 +21,34 @@ function Header() {
     )
 }
 
+function CoreConcept({ image, title, description }) {
+    return (
+        <li>
+            <img src={image} alt={''} />
+            <h3>{title}</h3>
+            <p>{description}</p>
+        </li>
+    )
+}
+
 function App() {
   return (
     <div>
       <Header />
       <main>
+          <section id="core-concepts">
+              <h2>Core Concepts</h2>
+              <ul>
+                  <CoreConcept
+                      title={CORE_CONCEPTS[0].title}
+                      description={CORE_CONCEPTS[0].description}
+                      image={CORE_CONCEPTS[0].image}
+                  />
+                  <CoreConcept {...CORE_CONCEPTS[1]} />
+                  <CoreConcept {...CORE_CONCEPTS[2]} />
+                  <CoreConcept {...CORE_CONCEPTS[3]} />
+              </ul>
+          </section>
         <h2>Time to get started!</h2>
       </main>
     </div>
