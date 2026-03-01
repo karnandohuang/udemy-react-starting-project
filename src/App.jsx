fbd1a2c39ea2817b28f6764834f50469229d646b
karnandohuang <karnandohuang@outlook.com>
2026-03-01 22:08:35 +0700

    remove unused imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
-import { useState } from 'react'
-import {EXAMPLES} from './data-with-examples'
 import Header from "./components/Header/Header";
 import CoreConcepts from "./components/CoreConcepts";
-import TabButton from "./components/TabButton";
 import Examples from "./components/Examples";
 
 function App() {
